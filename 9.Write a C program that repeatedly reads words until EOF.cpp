cd04d0a29a6422cc2773d36409059429d32758b5
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-12-02 01:28:29 +0600

    Create 9.Write a C program that repeatedly reads words until EOF.cpp

---
+++ 9.Write a C program that repeatedly reads words until EOF.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <set>
+using namespace std;
+
+set<string> keywords = {
+    "int", "if", "else", "for", "while", "return", "char", "float", "double", "void"};
+
+bool isIdentifier(const string &word)
+{
+    if (word.empty())
+        return false;
+
+    if (!isalpha(word[0]) && word[0] != '_')
+        return false;
+
+    for (size_t i = 1; i < word.length(); i++)
+    {
+        if (!isalnum(word[i]) && word[i] != '_')
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    string word;
+
+    cout << "Enter words (Ctrl+D to end input on Unix, Ctrl+Z on Windows):\n";
+
+    while (cin >> word)
+    {
+        if (keywords.count(word))
+        {
+            cout << word << " : Keyword\n";
+        }
+        else if (isIdentifier(word))
+        {
+            cout << word << " : Valid Identifier\n";
+        }
+        else
+        {
+            cout << word << " : Invalid Token\n";
+        }
+    }
+
+    return 0;
+}
